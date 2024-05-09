10d28cc3b5f9965bc85de331d104176e8cdd82ba
Rhuan Sassi <rhuansassi@gmail.com>
2024-05-09 01:39:26 -0300

    Migrations with FlyWay

---
+++ 11_MigrationsWithFlyWay/src/main/resources/db/migration/V2__Populate_Table_person.sql
@@ -0,0 +1,7 @@
+-- Copiando dados para a tabela rest_with_spring_boot_rhuan.person: ~4 rows (aproximadamente)
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Santa Maria - Brazil', 'Rhuan', 'Male', 'Sassi'),
+	(2, 'Porto Alegre - Brazil', 'João', 'Male', 'Silva'),
+	(4, 'EUA', 'Michael', 'Male', 'Jackson'),
+	(5, 'EUA', 'Ben', 'Male', 'Afleck'),
+	(6, 'EUA', 'John', 'Male', 'Kennedy');
