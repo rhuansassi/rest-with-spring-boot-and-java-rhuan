7f035eeb27767a8de05a9081a7c40ab67b07e74b
Rhuan Sassi <rhuansassi@gmail.com>
2024-05-09 02:23:27 -0300

    Custom Serealization

---
+++ 12_CustomSerealization/src/main/java/br/com/rhuanjava/data/vo/v1/PersonVO.java
@@ -0,0 +1,95 @@
+package br.com.rhuanjava.data.vo.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.util.Objects;
+@JsonPropertyOrder({"id", "first_name","last_name","gender","address"})
+public class PersonVO {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String address;
+    @JsonIgnore
+    private String gender;
+
+    public PersonVO() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonVO person = (PersonVO) o;
+
+        if (!Objects.equals(id, person.id)) return false;
+        if (!Objects.equals(firstName, person.firstName)) return false;
+        if (!Objects.equals(lastName, person.lastName)) return false;
+        if (!Objects.equals(address, person.address)) return false;
+        return Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        return result;
+    }
+
+}
