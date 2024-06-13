3dc2b70755c9ff4e841098cea01eed5c5933cd70
Rhuan Sassi <rhuansassi@gmail.com>
2024-06-13 20:54:43 -0300

    Working with CORS

---
+++ 16_CORS/src/main/java/br/com/rhuanjava/services/BookServices.java
@@ -0,0 +1,82 @@
+package br.com.rhuanjava.services;
+
+import br.com.rhuanjava.controllers.BookController;
+import br.com.rhuanjava.data.vo.v1.BookVO;
+import br.com.rhuanjava.exceptions.RequiredObjectIsNullException;
+import br.com.rhuanjava.exceptions.ResourceNotFoundException;
+import br.com.rhuanjava.mapper.DozerMapper;
+import br.com.rhuanjava.model.Book;
+import br.com.rhuanjava.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class BookServices {
+
+    private Logger logger = Logger.getLogger(BookServices.class.getName());
+
+    @Autowired
+    BookRepository repository;
+
+    public List<BookVO> findAll() {
+
+        logger.info("Finding all books!");
+        var books = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
+        books.
+                stream()
+                .forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+        return books;
+    }
+
+    public BookVO findById(Long id) {
+
+        logger.info("Find one book!");
+
+        var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        var vo = DozerMapper.parseObject(entity, BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+        return vo;
+    }
+
+    public BookVO create(BookVO book) {
+
+        if (book == null) throw new RequiredObjectIsNullException();
+        logger.info("Creating one Book!");
+        var entity = DozerMapper.parseObject(book, Book.class);
+        var vo = DozerMapper.parseObject(repository.save(entity), BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public BookVO update(BookVO book) {
+
+        if (book == null) throw new RequiredObjectIsNullException();
+        logger.info("Updating one book!");
+        var entity = repository.findById(book.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        
+        entity.setAuthor(book.getAuthor());
+        entity.setTitle(book.getTitle());
+        entity.setPrice(book.getPrice());
+        entity.setLaunchDate(book.getLaunchDate());
+
+        var vo =  DozerMapper.parseObject(repository.save(entity), BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one book!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        repository.delete(entity);
+    }
+
+}
